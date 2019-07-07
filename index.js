6909de239976856f50efc8e7adac3b0d42f3aa55
draco823 <52636603+draco823@users.noreply.github.com>
2019-07-07 22:37:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg = require('./index.json');
-const token = process.env.token
+const token = process.env.token;
 const prefix = ("/");
 
 bot.on('ready', function () {
