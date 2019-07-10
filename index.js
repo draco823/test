248aebf66fc705a18c5fef02834cd68efe454c12
draco823 <52636603+draco823@users.noreply.github.com>
2019-07-10 16:21:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ bot.on('message', msg => {
     }
     if (msg.content === prefix + "partenaires"){
         msg.reply("voici les partenaires d'**__Element Hall__** :")
-        msg.channel.send("**Factrio** : https://discord.gg/GhPm74m")
         msg.channel.send("**Des Gens** : https://discord.gg/Ahu6Jrm")
         msg.channel.send("**ailedeuxplume Communauté** : https://discord.gg/xr9BWpR")
         msg.channel.send("**Math2032 Officiel** : https://discord.gg/AR89kdE")
